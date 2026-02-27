77d313c65abbfe683753139d693a68e9a1995975
Jamie <jamie@wakeflow.io>
2026-02-27 15:15:27 +0000

    ENV_FILE

--- scripts/generate-standup.js
+++ scripts/generate-standup.js
@@ -1,5 +1,7 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import path from 'path'
+
+dotenv.config({ path: process.env.ENV_FILE || '.env' })
 import dayjs from 'dayjs'
 import { run } from '../src/commits-report.js'
 
